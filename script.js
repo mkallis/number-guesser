0705b6d5ba6f8d3794b88eac96f34c9cb4e02f00
Megan Kallis <megankallis@gmail.com>
2023-06-23 11:12:17 -0700

    Write function to generate new target value

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+// Returns a random integer between 0 and 9 
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
+
+console.log(generateTarget());
