faa75d0019e6f3304b11c1e6c01702b432d1376b
Megan Kallis <megankallis@gmail.com>
2023-06-23 11:29:10 -0700

    Adds function to compare human guess and computer guess to determine who wins

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ const generateTarget = () => {
 }
 
 console.log(generateTarget());
+
+
+// Compares human guess, computer guess, and the secret target number to be guessed
+const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+    humanDistance = Math.abs(humanGuess - targetNumber);
+    computerDistance = Math.abs(computerGuess - targetNumber);
+
+    if (humanDistance < computerDistance) {
+        console.log('human wins');
+        return true;
+    }
+    else if (computerDistance < humanDistance) {
+        console.log('computer wins');
+        return false;
+    }
+    else {
+        console.log('it is a tie!');
+        return true;
+    }
+}
+
+// computer wins
+console.log(`4, 6, 8`);
+console.log(compareGuesses(4, 6, 8));
+
+// human wins
+console.log(`4, 6, 2`);
+console.log(compareGuesses(4, 6, 2));
+
+// tie
+console.log(`4, 6, 5`);
+console.log(compareGuesses(4, 6, 5));
