c7574f2d0eb03f5117fa9a6e3519896a1e43f81f
Megan Kallis <megankallis@gmail.com>
2023-06-23 11:42:34 -0700

    remove console.logs

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
 
 // Returns a random integer between 0 and 9 
 const generateTarget = () => {
     return Math.floor(Math.random() * 10);
 }
-
-// console.log(generateTarget());
 
 
 // Compares human guess, computer guess, and the secret target number to be guessed
@@ -18,53 +15,26 @@ const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
     computerDistance = Math.abs(computerGuess - targetNumber);
 
     if (humanDistance <= computerDistance) {
-        console.log('human wins');
         return true;
     }
     else if (computerDistance < humanDistance) {
-        console.log('computer wins');
         return false;
     }
 }
-
-// computer wins
-// console.log(`4, 6, 8`);
-// console.log(compareGuesses(4, 6, 8));
-
-// human wins
-// console.log(`4, 6, 2`);
-// console.log(compareGuesses(4, 6, 2));
-
-// tie, but human wins
-// console.log(`4, 6, 5`);
-// console.log(compareGuesses(4, 6, 5));
 
 
 // Updates score based on which player won
 const updateScore = (winner) => {
     if (winner === 'human') {
         humanScore++;
-        console.log('human score increased');
     } 
     else if (winner === 'computer') {
         computerScore++;
-        console.log('computer score increased');
     }
 }
-
-// pass in human
-// console.log(updateScore('human'));
-// pass in computer
-// console.log(updateScore('computer'));
 
 
 // Updates the round number after each round played
 const advanceRound = () => {
     currentRoundNumber++;
-    console.log('round number increased');
-    console.log(currentRoundNumber);
 }
-
-console.log('initial round number', currentRoundNumber);
-console.log('increase round number');
-console.log(advanceRound());
